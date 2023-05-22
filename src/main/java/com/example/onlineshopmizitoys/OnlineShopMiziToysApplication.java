41d08a3ee8e1f76e6e82ca02520533e8de9ef7e3
HomeUserProject <eugene.chernitsyn15@gmail.com>
2023-05-22 15:11:34 +0400

    Initial commit

---
+++ src/main/java/com/example/onlineshopmizitoys/OnlineShopMiziToysApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopmizitoys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopMiziToysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopMiziToysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlineshopmizitoys/OnlineShopMiziToysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopmizitoys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopMiziToysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
